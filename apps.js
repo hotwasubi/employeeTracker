ee7f17967f3ec929059d37c836d1c79717679c02
Antknee <anthonycolindres@yahoo.com>
2020-03-29 15:27:02 -0400

    sent up connection

--- apps.js
+++ apps.js
@@ -1,3 +1,22 @@
-const inquirer = require('inquier');
+const inquirer = require('inquirer');
 const mysql = require('mysql');
-const consoletable = require('console.table') 
+const consoletable = require('console.table');
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: "root",
+    password: "Phoebe12@",
+    database: "employee_DB"
+})
+
+connection.connect((err) => {
+    if(err){
+        console.log(`error connecting`, err.stack);
+        return
+    }
+    console.log(`Connected through id ${connection.threadId}`);
+})
+connection.end();
