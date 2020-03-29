ee23f5e3aa73014cd7fffd0c69350d067e183ece
Antknee <anthonycolindres@yahoo.com>
2020-03-29 16:50:46 -0400

    WIP

--- apps.js
+++ apps.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Phoebe12@",
-    database: "employee_DB"
+    database: "employee_db"
 })
 
 connection.connect((err) => {
@@ -31,8 +31,100 @@ function starterPrompt (){
         console.log(chalk.red(data))
     });
 
+    inquirer.prompt([{
+        message: 'What would you like to do?',
+        type: 'list',
+        name: 'Choice',
+        choices: [
+            'View Employees',
+            'View Employees by Department',
+            'View Employees by Role',
+            'Add new Employee',
+            'Add Role',
+            'Add Department',
+            'Update Employee',
+            'View Roles',
+            'View Departments',
+            'Exit'
+        ]
+    }])
 
+    .then((answers) => {
+        if (answers.choice === 'View Employees') {
+            viewEmployees();
+        } else if (answers.choice === 'View Employees by Department') {
+            viewByDept();
+        } else if (answers.choice === 'View Employees by Role') {
+            viewByRole();
+        } else if (answers.choice === 'Add Employee') {
+            addEmployee();
+        } else if (answers.choice === 'Add Role') {
+            addRole();
+        } else if (answers.choice === 'Add Department') {
+            addDept();
+        } else if (answers.choice === 'Update Employee') {
+            updateEmployeeRole();
+        } else if (answers.choice === 'View Roles') {
+            viewRoles();
+        } else if (answers.choice === 'View Departments') {
+            viewDepts();
+        }
+
+        else if (answers.choice === 'Exit') {
+            exit();
+        }
+    })
 };
 
 starterPrompt();
+
+// SQL  
+
+function viewEmployees() {
+    connection.query(`SELECT employee.first_name,
+    employee.last_name,
+    role.title AS Title,
+    role.salary AS salary,
+    department.name AS Department
+FROM employee
+    INNER JOIN role ON employee.role_id=role.role_id
+    INNER JOIN department ON employee.role_id=department.department_id`, (err, result) =>{
+      if (err) throw err;
+      console.table(result);  
+    })
+    starterPrompt();
+};
+
+function viewByDept() {
+    inquirer.prompt([{
+        message: "Which department would you like to view?",
+        type: 'list',
+        name: 'dept',
+        choices: [
+            'Engineering',
+            'Management',
+            'Marketing',
+            'Legal',
+            'Sales'
+        ]
+    }])
+        .then((answers) => {
+            connection.query(`SELECT 
+            employee.first_name,
+            employee.last_name,
+            department.name AS Department
+        FROM employee
+            INNER JOIN role ON employee.role_id=role.role_id     
+            INNER JOIN department ON employee.role_id=department.department_id
+            WHERE department.name='${answers.dept}'`, (err, result) => {
+                if (err) throw err;
+                console.table(result);
+
+            })
+            starterPrompt();
+        })
+
+};
+
+
 connection.end();
